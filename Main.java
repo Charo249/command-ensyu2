4f3e67a42447f2692b1cbdcb9af9d4972b65a45c
take11 <take.sato.sys22@morijyobi.ac.jp>
2022-11-09 15:03:28 +0900

    二人分に修正

--- Main.java
+++ Main.java
@@ -1,28 +1,18 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Scanner;
-public class Main {
-        public static void main(String args[]) {
-             
-          BigDecimal bd;
-          
           Scanner stdIn = new Scanner(System.in);
-             
-                System.out.println("BMI‚ÌŒvZ");
-             
-                System.out.print("Eg’·(m)‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
-                double height = stdIn.nextDouble(); 
-                System.out.print("E‘Ìd(Kg)‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
-                
-                double weight = stdIn.nextDouble(); 
-             
-                double bmi = weight / Math.pow(height, 2);        
-                
-                System.out.println("BMI’l‚Í" + (Math.floor(bmi*100))/100);
-  
-              }
-            }                       
-        
 
+                System.out.println("BMI<82>Ì<8c>v<8e>Z");
+
+                System.out.print("<81>E<90>g<92>·(m)<82>ğ<93>ü<97>Í<82>µ<82>Ä<82>­<82>¾<82>³<82>¢<81>F");
+                double height = stdIn.nextDouble();
+                System.out.print("<81>E<91>Ì<8f>d(Kg)<82>ğ<93>ü<97>Í<82>µ<82>Ä<82>­<82>¾<82>³<82>¢<81>F");
+
+                double weight = stdIn.nextDouble();
+
+                double bmi = weight / Math.pow(height, 2);
+
+                System.out.println("BMI<92>l<82>Í" + (Math.floor(bmi*100))/100);
+
+              }
+            }
 
 
